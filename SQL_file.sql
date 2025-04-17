5ce9776ab9a219a130cb0a5ad2824b1ff9f3220f
APEH ADAMS <adamsapeh35@gmail.com>
2025-04-17 10:47:45 +0100

    Update SQL_file.sql

--- SQL_file.sql
+++ SQL_file.sql
@@ -1,15 +1,15 @@
--- Question 1 🗑️
+-- Question 1
 -- Drop the index named 'IdxPhone' from the 'customers' table
 DROP INDEX IdxPhone ON customers;
 
--- Question 2 👤
+-- Question 2
 -- Create a user named 'bob' with the password 'S$cu3r3!', restricted to localhost
 CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
 
--- Question 3 🔑
+-- Question 3
 -- Grant INSERT privilege to 'bob' on the 'salesDB' database
 GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
 
--- Question 4 🔐
+-- Question 4
 -- Change the password for the user 'bob' to 'P$55!23'
 ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
